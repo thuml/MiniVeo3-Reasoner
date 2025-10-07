434cde85475a797ecb2603889a9d38eee2a39a9d
wujialong <manchery@outlook.com>
2025-10-07 09:24:28 +0000

    update evaluation

--- scripts/inference_maze_testset.sh
+++ scripts/inference_maze_testset.sh
@@ -1,6 +1,6 @@
 # This inference script is for the Maze Reasoning dataset, automatically evaluating all sub-categories.
 
-dir="dataset/test"
+dir="dataset/maze_test"
 
 for sub in "maze3x3" "maze4x4" "maze5x5" "maze6x6" "maze7x7" "maze8x8" "maze6x6_ood"; do
     python inference/maze/inference_maze.py -r "${dir}/${sub}/" --quiet
