32191d331e83099bd4a1ac1d3e2441405518cba3
hcjqwq <hcj24@mails.tsinghua.edu.cn>
2025-10-07 02:27:41 +0000

    fixed bugs in evaluation

--- evaluation/maze/identify_traj.py
+++ evaluation/maze/identify_traj.py
@@ -2,9 +2,6 @@ import cv2
 import numpy as np
 
 def keep_blue_hsv(frame):
-    """
-    使用HSV颜色空间保留蓝色部分
-    """
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     lower_blue = np.array([90, 100, 50])
     upper_blue = np.array([130, 255, 255])
@@ -13,15 +10,10 @@ def keep_blue_hsv(frame):
     return result, mask
 
 def find_center(mask):
-    """
-    找到星星的中心位置
-    """
     points = cv2.findNonZero(mask)
     
     if points is None or len(points) < 20:
-        return None  # 没有找到蓝色像素
-    
-    # 计算所有非零像素的质心（中心点）
+        return None  
     moments = cv2.moments(mask)
     
     if moments["m00"] != 0:
@@ -32,20 +24,15 @@ def find_center(mask):
         return None
 
 def process_video_with_trajectory(input_path, output_path):
-    """
-    处理视频并追踪星星轨迹
-    """
-    #import pdb;pdb.set_trace()
     cap = cv2.VideoCapture(input_path)
     fps = int(cap.get(cv2.CAP_PROP_FPS))
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     
-    # 创建输出视频
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter("./debug.mp4", fourcc, fps, (width, height))
+    #out = cv2.VideoWriter("./debug.mp4", fourcc, fps, (width, height)) # print out the video of identfied traj
     
-    trajectory = []  # 存储轨迹点
+    trajectory = [] 
     
     while True:
         ret, frame = cap.read()
@@ -58,31 +45,20 @@ def process_video_with_trajectory(input_path, output_path):
         if center:
             trajectory.append(center)
             
-            # 在视频上绘制轨迹
-            for i in range(1, len(trajectory)):
-                cv2.line(blue_frame, trajectory[i-1], trajectory[i], (0, 255, 0), 2)
+        #     for i in range(1, len(trajectory)):
+        #         cv2.line(blue_frame, trajectory[i-1], trajectory[i], (0, 255, 0), 2)
             
-            # 标记当前中心点
-            cv2.circle(blue_frame, center, 8, (0, 0, 255), -1)
+        #     cv2.circle(blue_frame, center, 8, (0, 0, 255), -1)
         
-        out.write(blue_frame)
+        # out.write(blue_frame)
     
     cap.release()
-    out.release()
-    
-    # 打印轨迹信息
-    # print(f"轨迹点数量: {len(trajectory)}")
-    # for i, (x, y) in enumerate(trajectory):
-    #     print(f"帧 {i}: 中心位置 ({x}, {y})")
+    # out.release()
     
     return trajectory
 
-# 使用示例
 def gen_traj(file_path1,file_path2):
     expert = process_video_with_trajectory(file_path1,"1.mp4")
     student = process_video_with_trajectory(file_path2,"2.mp4")
     return expert,student 
 
-
-# trajectory = track_blue_star("/data/NAS/rl_data/video_reasoner/huangtianhao_data/DiffSynth-Studio/inference_data/test3by3/maze3_1251_epoch-2_inference.mp4","2.mp4")
-# print(trajectory)
